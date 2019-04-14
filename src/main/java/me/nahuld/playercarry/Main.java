d959d3e9c862f5ac11eccceb0af43ece947392e6
nahuel <nahueldolores@hotmail.com>
2019-04-14 20:58:10 -0300

    Init commit

---
+++ src/main/java/me/nahuld/playercarry/Main.java
@@ -0,0 +1,16 @@
+package me.nahuld.playercarry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //TODO: stuff
+    }
+
+    @Override
+    public void onDisable() {
+        //TODO: stuff
+    }
+}
